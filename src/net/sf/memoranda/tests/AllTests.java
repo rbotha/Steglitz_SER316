f46388c9f227d0cea4f23a38d2d1412b510a9eb5
rbotha <rbotha@asu.edu>
2017-04-10 18:07:06 -0700

    Added test suite

---
+++ src/net/sf/memoranda/tests/AllTests.java
@@ -0,0 +1,11 @@
+package net.sf.memoranda.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ContactsTest.class, EmailTest.class, ProjectImplTest.class, ResourcesImplTest.class })
+public class AllTests {
+
+}
