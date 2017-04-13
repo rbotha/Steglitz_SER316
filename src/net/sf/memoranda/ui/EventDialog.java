4f3eca46ba4627c7b68b9b0b27b0957fbdcbb550
Will <wsamuels@asu.edu>
2017-04-12 21:57:21 -0500

    forgot to add some imports

--- src/net/sf/memoranda/ui/EventDialog.java
+++ src/net/sf/memoranda/ui/EventDialog.java
@@ -37,6 +37,15 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+import javax.swing.WindowConstants;
 import net.sf.memoranda.date.CalendarDate;
 import net.sf.memoranda.util.Local;
 import javax.swing.BoxLayout;
